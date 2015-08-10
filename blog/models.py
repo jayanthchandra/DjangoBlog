00af246ee8354e0f7bf93c3b7e8cd56c2c07907c
jayanthchandra <jayanthchandra14@gmail.com>
2015-08-10 23:27:29 +0530

    First Commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+	"""docstring for Post"""
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+
+	def __str__(self):
+		return self.title
+
+		
